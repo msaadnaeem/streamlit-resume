f1e4754a486a2c7a73a5fda1941e69b3d318c19b
msaadnaeem <110536997+msaadnaeem@users.noreply.github.com>
2022-10-27 20:34:09 +0500

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,36 @@
+from turtle import width
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import plotly.express as px
+colLeft,colRight=st.columns(2)
+with colLeft:
+    st.title('Resume')
+    st.header('Muhammad Saad Naeem')
+
+with colRight:
+    pic=Image.open('dp.png')
+    st.image(pic,width=175)   
+
+
+colLeft,colRight=st.columns(2)
+with colLeft:
+    st.subheader('Bio Data')
+    st.write('Name: Muhammad Saad Naeem')
+    st.write('Email: rsnthesmart@gmail.com')
+
+with colRight:
+    st.subheader('Education')
+    st.write('Masters: Oxford University')
+    st.write('Bachelors: Oxford University')
+
+skills={
+    'Skills':['Python','SQL','Excel'],
+    'Levels':[30,20,50]
+}
+
+skillsDf=pd.DataFrame(skills)
+st.write(skillsDf)
+
+fig=px.pie(skillsDf, values='Levels', names='Skills', title='My Skills')
+st.plotly_chart(fig)
